3a24aedf549f92f642d3756e4c816680975e234b
zikr444 <zikrimaulana873>
2023-10-24 20:58:05 +0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { Routes, Route } from "react-router-dom"
-
+import React from "react"
+import { Routes, Route, Navigate } from "react-router-dom"
 import NavbarComponent from "./components/NavbarComponent"
-
 import FooterComponent from "./components/FooterComponent"
-
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage" // Make sure to import your HomePage component.
 import KelasPages from "./pages/KelasPages"
 import TestimonialPages from "./pages/TestimonialPages"
 import FaqPage from "./pages/FaqPage"
@@ -14,16 +12,14 @@ function App() {
   return (
     <div>
       <NavbarComponent />
-
       <Routes>
         <Route path="/zikri-course" element={<HomePage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/zikri-course" />} />
         <Route path="/kelas" element={<KelasPages />} />
         <Route path="/testimonial" element={<TestimonialPages />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/syaratketen" element={<SyaratKetenPage />} />
       </Routes>
-
       <FooterComponent />
     </div>
   )
